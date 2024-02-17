51186fffb517015235706bffed327a8d4641dd56
IT-Admin <IT-Admin@lenovo-PC>
2024-02-17 18:02:35 +0530

    commit

---
+++ IntroductionToJava/src/com/capgemini/introduction/HelloWorldDemo.java
@@ -0,0 +1,10 @@
+package com.capgemini.introduction;
+//program to demonstrate on printing a statement
+public class HelloWorldDemo {
+
+	public static void main(String[] args) {
+		System.out.println("Hey, THERE I AM WRITING HELLO WORLD PROGRAM");
+
+	}
+
+}
